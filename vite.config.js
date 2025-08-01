e498c3af4f401817510d252cfca73a2aae6312fb
staubjoao <joao.staub42@gmail.com>
2025-07-31 22:05:42 -0300

    publish page

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: './', 
   build: {
+    outDir: 'dist', 
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html')
+        main: path.resolve(__dirname, 'index.html'),
+        graph: path.resolve(__dirname, 'graph.js'),
+        form: path.resolve(__dirname, 'form.js'),
+        classes: path.resolve(__dirname, 'classes.js'),
+        mainJS: path.resolve(__dirname, 'main.js')
+      },
+      output: {
+        assetFileNames: 'assets/[name].[ext]', 
       }
     }
   }
